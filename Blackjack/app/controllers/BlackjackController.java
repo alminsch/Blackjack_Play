7bb0b44b77a988ba0f4a6a56e473aae47dcffcbc
Benjamin Herrmann <conquiztadoracc@web.de>
2016-11-16 12:57:54 +0100

    commit

--- Blackjack/app/controllers/BlackjackController.java
+++ Blackjack/app/controllers/BlackjackController.java
@@ -2,7 +2,12 @@ package controllers;
 
 import play.mvc.*;
 import views.html.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import de.htwg.blackjack.Blackjack;
+import de.htwg.blackjack.controller.IController;
 import de.htwg.blackjack.view.tui.TextUI;
 
 public class BlackjackController extends Controller {
@@ -13,5 +18,16 @@ public class BlackjackController extends Controller {
 	}
 	public Result spielregeln() {
 		return ok(blackjack.render());
+	}
+	
+	public Result index() {
+    	IController controller = Blackjack.getInstance().getController();
+        return ok(index.render("Your new application is ready.",controller));
+    }
+	
+	public static Result json() {
+	Map<String, Object> obj = new HashMap<String, Object>();
+	return null;
+	
 	}
 }
